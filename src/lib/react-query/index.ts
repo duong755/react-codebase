945587ccf02dda3c58f526f10ea7e8037c166185
duong755 <ngo.quang.duong.1100@gmail.com>
2023-04-08 15:12:29 +0700

    chore: install patch version of minimatch, add react-query, react-hook-form

---
+++ src/lib/react-query/index.ts
@@ -0,0 +1,16 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const defaultQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      retryOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+  logger: console,
+});
