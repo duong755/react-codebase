9754dce622676c51e52315e08d789110ca564dbe
duong755 <ngo.quang.duong.1100@gmail.com>
2023-04-08 14:25:33 +0700

    fix: unmount component after each test

--- src/screens/about/about.test.tsx
+++ src/screens/about/about.test.tsx
@@ -1,6 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 
 import { AboutScreen } from "./about";
+
+afterEach(() => {
+  cleanup();
+  jest.clearAllMocks();
+});
 
 describe("test about page", () => {
   it("should display title", async () => {
